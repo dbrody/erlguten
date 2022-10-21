67589e5913222c147b874526fb000faaaa4b1a37
David Brody <dbrody@gmail.com>
2022-10-20 18:58:40 -0700

    Remove non GB languages

--- src/eg_hyphenate.erl
+++ src/eg_hyphenate.erl
@@ -38,11 +38,7 @@ test(1) -> word("hyphenation", eg_hyphen_rules_en_GB);
 test(2) -> partitions("supercalifragilisticexpialidocious", 
 		      eg_hyphen_rules_en_GB);
 test(3) -> test_text(eg_hyphen_rules, text_en()); %% using old .tex based rules 
-test(4) -> test_text(eg_hyphen_rules_sv_SE, text_sv());
 test(5) -> test_text(eg_hyphen_rules_en_GB, text_en());
-test(6) -> test_text(eg_hyphen_rules_nb_NO, text_nb());
-test(7) -> test_text(eg_hyphen_rules_fi_FI, text_fi());
-test(8) -> test_text(eg_hyphen_rules_da_DK, text_da()).
 
 
 test_text(Rules, Text) -> 
@@ -62,18 +58,18 @@ get_words(Text) ->
 %% text from: Svenska Dagbladet (2009-08-05) 
 %%            http://www.svd.se/nyheter/inrikes/artikel_3310817.svd
 text_sv() ->
-    "Ali Mehrabi och hans kompisar skulle överlämna ett brev och en "
-	"film till UD igår. De fick träffa Anders Jörle, presschef för "
-	"UD:s presstjänst, han skulle se till att utrikesminister Carl "
-	"Bildt fick materialet. I brevet vädjar ungdomarna till ministern "
-	"att inte erkänna Mahmoud Ahmadinejad som Irans president. "
+    "Ali Mehrabi och hans kompisar skulle ï¿½verlï¿½mna ett brev och en "
+	"film till UD igï¿½r. De fick trï¿½ffa Anders Jï¿½rle, presschef fï¿½r "
+	"UD:s presstjï¿½nst, han skulle se till att utrikesminister Carl "
+	"Bildt fick materialet. I brevet vï¿½djar ungdomarna till ministern "
+	"att inte erkï¿½nna Mahmoud Ahmadinejad som Irans president. "
 
 	"Folkpartiets utrikespolitiske talesperson, Birgitta Ohlsson, "
 	"tycker att Sverige borde bli tuffare i sin kritik mot den Iranska "
-	"regimen och att man borde stödja demokratirörelsen i landet. "
-	"Hon påpekar dock att Sverige representerade EU på "
-	"presidentinstallationen och att meningarna där var delade när "
-	"det gäller ceremonin.".
+	"regimen och att man borde stï¿½dja demokratirï¿½relsen i landet. "
+	"Hon pï¿½pekar dock att Sverige representerade EU pï¿½ "
+	"presidentinstallationen och att meningarna dï¿½r var delade nï¿½r "
+	"det gï¿½ller ceremonin.".
 
 %% text from: BBC (2009-08-05) 
 %%            http://news.bbc.co.uk/2/hi/middle_east/8184240.stm
@@ -96,11 +92,11 @@ text_en() ->
 text_nb() ->
     "Hvordan hadde du forholdt deg til en \"Ringenes Herre\"-filmatisering "
 	"med Paul McCartney som Frodo Baggins, John Lennon som Gandalf the "
-	"Grey og Stanley Kubrick som regissør? "
-	"Det høres ut som et meget surrealistisk og uvirkelig prosjekt, men "
-	"det kunne fort ha vært en realitet. "
+	"Grey og Stanley Kubrick som regissï¿½r? "
+	"Det hï¿½res ut som et meget surrealistisk og uvirkelig prosjekt, men "
+	"det kunne fort ha vï¿½rt en realitet. "
 	"Tolkien-eksperten Michael D. C. Drout skriver blant annet i boka "
-	"si \"J.R.R. Tolkien encyclopedia\" hvordan The Beatles prøvde å "
+	"si \"J.R.R. Tolkien encyclopedia\" hvordan The Beatles prï¿½vde ï¿½ "
 	"lage film av Tolkien-romanen \"Ringenes Herre\" etter at "
 	"rettighetene var solgt til United Artists i 1968. "
 	"Tanken deres var at The Beatles selv skulle spille hovedrollene "
@@ -110,34 +106,34 @@ text_nb() ->
 %%            http://jp.dk/udland/usa/article1772552.ece
 text_da() ->
     "To amerikanske journalister tilbageholdt i Nordkorea i 140 dage "
-	"brød sammen ved synet af deres familie i USA. Det er godt at "
-	"være fri, siger de. "
-	"I første omgang så Hana Lee lidt forvirret ud. "
-	"Den fireårige pige havde ikke set sin mor i 140 dage. "
-	"Men så knugede hun sig fast til den amerikanske journalist Euna "
+	"brï¿½d sammen ved synet af deres familie i USA. Det er godt at "
+	"vï¿½re fri, siger de. "
+	"I fï¿½rste omgang sï¿½ Hana Lee lidt forvirret ud. "
+	"Den fireï¿½rige pige havde ikke set sin mor i 140 dage. "
+	"Men sï¿½ knugede hun sig fast til den amerikanske journalist Euna "
 	"Lee og ville ikke give slip igen."
-	"Sådan var scenen onsdag eftermiddag dansk tid, da Euna Lee og "
+	"Sï¿½dan var scenen onsdag eftermiddag dansk tid, da Euna Lee og "
 	"Laura Ling ankom til lufthavnen i Burbank, Californien."
-	"De to journalister har været tilbageholdt i Nordkorea, siden de "
-	"i marts blev anholdt ved grænsen mellem Nordkorea og Kina - og "
-	"siden blev idømt 12 år i en arbejdslejr for at have overskredet "
-	"grænsen ulovligt.".
+	"De to journalister har vï¿½ret tilbageholdt i Nordkorea, siden de "
+	"i marts blev anholdt ved grï¿½nsen mellem Nordkorea og Kina - og "
+	"siden blev idï¿½mt 12 ï¿½r i en arbejdslejr for at have overskredet "
+	"grï¿½nsen ulovligt.".
 
 %% text from: wikipedia (2009-08-06) (About finnish person numbers)
 %%            http://fi.wikipedia.org/wiki/Henkil%C3%B6tunnus
 text_fi() ->
-    "Suomessa henkilötunnus (hetu) annetaan Suomen kansalaisille sekä "
-	"Suomessa pysyvästi tai pitkäaikaisesti (vähintään vuoden) "
-	"oleskeleville ulkomaalaisille. Tunnus voidaan anomuksesta myöntää "
-	"myös tilapäisesti oleskeleville henkilöille. Tunnuksen myöntää "
-	"Väestörekisterikeskus. Henkilötunnusta ei voi käyttää luotettavasti "
-	"henkilöllisyyden todisteena kahdesta syystä: ensinnäkin se ei ole "
+    "Suomessa henkilï¿½tunnus (hetu) annetaan Suomen kansalaisille sekï¿½ "
+	"Suomessa pysyvï¿½sti tai pitkï¿½aikaisesti (vï¿½hintï¿½ï¿½n vuoden) "
+	"oleskeleville ulkomaalaisille. Tunnus voidaan anomuksesta myï¿½ntï¿½ï¿½ "
+	"myï¿½s tilapï¿½isesti oleskeleville henkilï¿½ille. Tunnuksen myï¿½ntï¿½ï¿½ "
+	"Vï¿½estï¿½rekisterikeskus. Henkilï¿½tunnusta ei voi kï¿½yttï¿½ï¿½ luotettavasti "
+	"henkilï¿½llisyyden todisteena kahdesta syystï¿½: ensinnï¿½kin se ei ole "
 	"salainen tieto, ja toiseksi se ei ole vaihdettavissa, "
 	"kuten vaikkapa verkkopankin salasana. Valitettavasti "
-	"henkilötunnusta, etenkin sen loppuosaa, usein luullaan "
-	"eräänlaiseksi \"salasanaksi\" vaikkapa pankkikorttiostoksia "
-	"tehdessä tai terveyskeskuksessa asioidessa, mikä johtaa helposti "
-	"väärinkäytöksiin.".
+	"henkilï¿½tunnusta, etenkin sen loppuosaa, usein luullaan "
+	"erï¿½ï¿½nlaiseksi \"salasanaksi\" vaikkapa pankkikorttiostoksia "
+	"tehdessï¿½ tai terveyskeskuksessa asioidessa, mikï¿½ johtaa helposti "
+	"vï¿½ï¿½rinkï¿½ytï¿½ksiin.".
 
 
 %% ============================================================================
